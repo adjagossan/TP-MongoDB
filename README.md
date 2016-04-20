8ea88b5337f8fc2c2c9dfa351fdce9e66bbf4077
adjagossan <adjagossan@gmail.com>
2016-04-20 05:47:52 +0200

    update README.md file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TP-NoSQL
+start MongoDB by executing from the command Prompt the command below and then run this Java application:
+
+`<your-mongodb-directory-path>\bin\mongod.exe --dbpath <your-mongodb-directory-path>\data\db`
+
+##### Limites d'une base de données orientées document
+pas de propriété ACID, ni de transactions
--- src/main/java/fr/istic/tpnosql/App.java
+++ src/main/java/fr/istic/tpnosql/App.java
@@ -7,76 +7,75 @@ import fr.istic.tpnosql.datastore.DataStoreHelper;
 import fr.istic.tpnosql.entity.*;
 import java.net.UnknownHostException;
 
-public class App 
+public class App
 {
     public static void main( String[] args ) throws UnknownHostException
     {
         String packageName = "fr.istic.tpnosql.entity";
         String dbName = "my_database";
         Datastore ds = DataStoreHelper.getInstance(packageName, dbName);
-        
+
         ArticleDao articleDao = new ArticleDao(Article.class, ds);
         PersonDao personDao = new PersonDao(Person.class, ds);
-        
+
         Article a = new Article();
         a.setName("panama papers");
         a.setStars(9);
-        
+
         Address ad = new Address();
         ad.setCity("Rennes");
         ad.setCountry("France");
         ad.setPostCode("35700");
         ad.setStreet("Beaulieu");
-        
+
         Address ad_ = new Address();
         ad_.setCity("Nantes");
         ad_.setCountry("France");
         ad_.setPostCode("92000");
         ad_.setStreet("Street");
-        
+
         Person pers = new Person();
         pers.setName("Aurelia");
-        
+
         pers.addAddress(ad);
         pers.addAddress(ad_);
-        
+
         Person pers_ = new Person();
         pers_.setName("Arielle");
-        
+
         Address ad__ = new Address();
         ad__.setCity("Abidjan");
         ad__.setCountry("Cote d'Ivoire");
         ad__.setPostCode("5000");
         ad__.setStreet("Babi");
-        
+
         pers_.addAddress(ad__);
-        
+
         a.addPerson(pers);
         a.addPerson(pers_);
-        
+
         personDao.save(pers);
         personDao.save(pers_);
         articleDao.save(a);
-        
+
         Article a_ = new Article();
         a_.setName("spacex");
         a_.setStars(5);
-        
+
         Person p = new Person();
         p.setName("Ingrid");
-        
+
         Address adr = new Address();
         adr.setCity("Yamoussoukro");
         adr.setCountry("Cote d'Ivoire");
         adr.setPostCode("300");
         adr.setStreet("Yakro");
-        
+
         p.addAddress(adr);
         a_.addPerson(p);
-        
+
         personDao.save(p);
         articleDao.save(a_);
-        
         
         QueryResults<Article> articles = articleDao.find(ds.createQuery(Article.class));
         for(Article ar : articles){
